b1804892f19a5c8bd978334929bfb3944132246b
sinae0421 <44888842+sinae0421@users.noreply.github.com>
2018-12-07 09:29:41 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+        $("#header").load("/include/header.html");
+        $("#footer").load("/include/footer.html");
+        $("#subHeader").load("/include/subHeader.html");
+        });
